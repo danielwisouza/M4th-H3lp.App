575145c9db2af74fb058c16a46d9391530bf5047
Daniel Willians Ignacio de Souza <daniel2wis@gmail.com>
2018-06-15 02:32:50 -0300

    Realizando mudança nos botões e criando tela Sobre

--- src/pages/sobre/sobre.ts
+++ src/pages/sobre/sobre.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
+
 /**
  * Generated class for the SobrePage page.
  *
@@ -22,5 +23,6 @@ export class SobrePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad SobrePage');
   }
+  
 
 }
